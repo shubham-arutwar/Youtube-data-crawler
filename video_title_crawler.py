f1fd101f14090e08cc23840454f108be99380777
Shubham Arutwar <shubhamarutwar23@gmail.com>
2023-06-14 01:07:25 +0530

    video transcription crawller and playlist video_list crawller done

--- video_title_crawler.py
+++ video_title_crawler.py
@@ -3,8 +3,9 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-channel_url = input("enter channel URL : ")
-url = str(channel_url) + "/videos"
+# channel_url = input("enter channel URL : ")
+# url = str(channel_url) + "/videos"
+url = input("enter playlist URL : ")
 
 def db_config(channel_name):
     client = MongoClient("mongodb+srv://cyanZEUS:cyanZEUS@cluster0.w4dydp9.mongodb.net/?retryWrites=true&w=majority")
@@ -22,22 +23,22 @@ def main():
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images": 2}
     chrome_options.add_experimental_option("prefs", prefs)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome()
     driver.maximize_window()
 
     driver.get(url)
     time.sleep(5)
     i = 1
-    while i < 2:
+    while i < 40:
         driver.execute_script("window.scrollBy(0,1000)","")
         print("scroll "+ str(i))
         time.sleep(3)
         i+=1
 
     content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, 'lxml')
-    ch_name = soup.find("yt-formatted-string", id="text", class_="style-scope ytd-channel-name")
-    titles = soup.findAll("a",id="video-title-link", class_="yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media")
+    soup = BeautifulSoup(content, 'html.parser')
+    ch_name = soup.find("yt-formatted-string", id="text", class_="style-scope yt-dynamic-sizing-formatted-string yt-sans-28")
+    titles = soup.findAll("a",id="video-title", class_="yt-simple-endpoint style-scope ytd-playlist-video-renderer")
     collection = db_config(replace_spaces(ch_name.text))
     print('\nChannel: {}\nURL: {}'.format(ch_name.text, url))
     i=1
--- video_transcription_crawler.py
+++ video_transcription_crawler.py
@@ -3,12 +3,31 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-url = "https://www.youtube.com/watch?v=hByfAzEoeKA"
-class1 = "style-scope ytd-transcript-segment-list-renderer"
-
+url = "https://www.youtube.com/watch?v=BgdxjmGN7B4"
 client = MongoClient("mongodb+srv://cyanZEUS:cyanZEUS@cluster0.w4dydp9.mongodb.net/?retryWrites=true&w=majority")
 
-def main():
+def db_trs_config(channel_name):
+    db = client[channel_name]
+    collection = db[""]
+    return collection
+
+# def save_to_db(collection, time_stamp, transcript):
+#     collection.insert_one({time_stamp : transcript})
+def save_to_trs_coll(collection, time_stamp, transcript):
+    existing_doc = collection.find_one()
+    if existing_doc:
+        collection.update_one({}, {"$set": {time_stamp: transcript.strip()}})
+    else:
+        collection.insert_one({time_stamp: transcript})
+
+    
+def replace_spaces(string):
+    return string.replace(" ", "_")
+
+def remove_spaces(string):
+    return string.replace(" ", "")
+
+def click_show_transcript():
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(2)
@@ -17,15 +36,29 @@ def main():
     show_transcript_btn = driver.find_element("xpath", '//yt-formatted-string[normalize-space()="Show transcript"]')
     show_transcript_btn.click()
     time.sleep(3)
-
     content = driver.page_source.encode('utf-8').strip()
     time.sleep(1)
-    soup = BeautifulSoup(content, 'lxml')
+    soup = BeautifulSoup(content, 'html.parser')
     time.sleep(1)
+    return soup
+
+def main():
+    soup = click_show_transcript()
+    ch_name = soup.find("yt-formatted-string", class_="style-scope ytd-channel-name complex-string")
+    v_title = soup.find("yt-formatted-string", class_="style-scope ytd-watch-metadata")
     time_stamp = soup.findAll("div", class_="segment-timestamp style-scope ytd-transcript-segment-renderer")
     transcript = soup.findAll("yt-formatted-string", class_="segment-text style-scope ytd-transcript-segment-renderer")
     time.sleep(1)
-    for x in time_stamp:
-        print('\n {} - {}'.format(x.text, transcript.text))
+    collection = db_config(replace_spaces(ch_name.text))
+    x = len(time_stamp)
+    i=0
+    while i < x:
+        print(remove_spaces(time_stamp[i].text.strip()))
+        print(transcript[i].text)
+        tm_s = remove_spaces(time_stamp[i].text.strip())
+        trs_tmp = transcript[i].text
+        trs = trs_tmp
+        save_to_trs_coll(collection, tm_s, trs)
+        i+=1
 
 main()
